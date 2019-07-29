c55c4558e0f0dceae0c15c9410551ebb157ac3e5
Alexander Nesterov <nesoldr@mail.ru>
2019-07-29 19:05:20 +0000

    update 29.07.2019

--- data_converter.py
+++ data_converter.py
@@ -3,7 +3,17 @@ import numpy as np
 
 def get_twitter_data_set():
     data_nel = open('new_data/TWITTER-Real-Graph-Partial.nel', 'r')
-    graph_size = 0
+    len_max = 0
+    for line in data_nel:
+        split_line = line.split()
+        if len(split_line) == 0:
+            continue
+        flag_nel = split_line[0]
+        if flag_nel == 'e':
+            len_max = max(len_max, int(split_line[1]), int(split_line[2]))
+    data_nel.close()
+
+    data_nel = open('new_data/TWITTER-Real-Graph-Partial.nel', 'r')
     graph = 0
     flag_create_matrix = True
 
@@ -16,18 +26,14 @@ def get_twitter_data_set():
         split_line = line.split()
 
         if len(split_line) == 0:
-            graph_size = 0
             flag_create_matrix = True
             continue
 
         flag_nel = split_line[0]
 
-        if flag_nel == 'n':
-            graph_size += 1
-
         if flag_nel == 'e':
             if flag_create_matrix:
-                graph = np.zeros((graph_size, graph_size)).astype(np.float32)
+                graph = np.zeros((len_max, len_max)).astype(np.float32)
                 flag_create_matrix = False
             graph[int(split_line[1]) - 1][int(split_line[2]) - 1] = np.float32(split_line[3])
 
@@ -36,19 +42,7 @@ def get_twitter_data_set():
             labels_graph.append(split_line[1])
             s.add(split_line[1])
 
-    len_max = len(data_graph[0])
-    for matr_new in data_graph:
-        len_max = max(len_max, len(matr_new))
-
-    new_data_graph = np.ndarray((len(data_graph), 1))
-    for matr_2 in data_graph:
-        edit_size = len_max - len(matr_2)
-        new_data_graph = np.append(new_data_graph, np.pad(matr_2, ((0, edit_size), (0, edit_size)), mode='constant', constant_values=(0, 0)))
-
-    new_labels_graph = np.ndarray((len(data_graph), 1))
-    new_labels_graph = np.append(new_labels_graph, labels_graph)
-
-    return {"data": new_data_graph, "labels": new_labels_graph, "label_values": s}
+    return {"data": data_graph, "labels": labels_graph, "label_values": s}
 
 
 def test_twitter_data_set():
@@ -107,5 +101,6 @@ def test_dblp_data_set():
 
 if __name__ == "__main__":
     dataset = get_twitter_data_set()
+    print("start check")
     for matr in dataset["data"]:
         assert len(matr) == len(dataset["data"][0])
