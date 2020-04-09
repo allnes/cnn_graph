6f430e55c325b910a55aebc8b2f1132a77b59974
Alexander Nesterov <nesoldr@gmail.com>
2020-04-09 06:16:56 +0300

    Update FP16

--- lib/graph.py
+++ lib/graph.py
@@ -22,10 +22,10 @@ def grid(m, dtype=np.float32):
 def distance_scipy_spatial(z, k=4, metric='euclidean'):
     """Compute exact pairwise distances."""
     z = z.astype(np.float16)
-    d = scipy.spatial.distance.pdist(z, metric).astype(np.float16)
-    d = scipy.spatial.distance.squareform(d).astype(np.float16)
+    d = scipy.spatial.distance.pdist(z, metric, dtype=float16)
+    d = scipy.spatial.distance.squareform(d, dtype=float16)
     # k-NN graph.
-    idx = np.argsort(d)[:, 1:k+1].astype(np.float16)
+    idx = np.argsort(d)[:, 1:k+1]
     d.sort()
     d = d[:, 1:k+1]
     return d, idx
