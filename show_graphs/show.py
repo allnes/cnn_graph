2b13565c91ce96586ef4514637a7d3b240810486
Alexander Nesterov <nesoldr@gmail.com>
2020-05-02 00:35:58 +0300

    added visualize Twitter

--- show_graphs/show.py
+++ show_graphs/show.py
@@ -2,47 +2,71 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 
-name_research = 'NCI Anti-cancer activity prediction data'
-data_dir = 'NCI'
-data_type = 'sdf'
+############################################################
+# name_research = 'NCI Anti-cancer activity prediction data'
+# data_dir = 'NCI'
+# data_type = 'sdf'
+############################################################
+name_research = 'Twitter Sentiment Graph Data'
+data_dir = 'Twitter'
+data_type = 'nel'
+###########################################################
 
 data_path = '{}/data.{}'.format(data_dir, data_type)
 title_size = {'fontsize': 40}
 count_images = 10
 coeff = 10
-
-for ind in range(count_images):
-    rows = cols = 2
-    fig = plt.figure(figsize=(rows * coeff, cols * coeff))
-    list_graphs = []
-
-    for i in range(rows):
-        list_graphs.append([])
-        for j in range(cols):
-            list_graphs[i].append(nx.Graph())
-
-    with open(data_path, 'r') as reader:
-        for i in range(rows):
-            for j in range(cols):
-                ############################################################
-                if data_type is 'sdf':
-                    pattern = r'\s*(\d*)\s*(\d*)\s*(\d*\s*)*V\d*'
-                    pattern_edge = r'\s*(\d*)\s*(\d*)(\s*\d*)*'
-                    for line in reader:
-                        result = re.search(pattern, line)
-                        if result is not None:
-                            for k in range(int(result.group(1))):
-                                reader.readline()
-                            for k in range(int(result.group(2))):
-                                result_edge = re.search(pattern_edge, reader.readline())
-                                list_graphs[i][j].add_edge(result_edge.group(1), result_edge.group(2))
-                            break
-                ############################################################
-                ############################################################
+with open(data_path, 'r') as reader:
+    for ind in range(count_images):
+        rows = cols = 2
+        fig = plt.figure(figsize=(rows * coeff, cols * coeff))
+        list_graphs = []
+        ############################################################
+        if data_type is 'sdf':
+            pattern_vertices = r'\s*(\d*)\s*(\d*)\s*(\d*\s*)*V\d*'
+            pattern_edge = r'\s*(\d*)\s*(\d*)(\s*\d*)*'
+            for line in reader:
+                result = re.search(pattern_vertices, line)
+                if result is not None:
+                    for k in range(int(result.group(1))):
+                        reader.readline()
+                    gx = nx.Graph()
+                    for k in range(int(result.group(2))):
+                        result_edge = re.search(pattern_edge, reader.readline())
+                        gx.add_edge(result_edge.group(1), result_edge.group(2))
+                    list_graphs.append(gx)
+                    if len(list_graphs) == rows * cols:
+                        break
+        ############################################################
+        if data_type is 'nel':
+            pattern_vertices = r'n\s*(\d*)\s(\w*)'
+            pattern_edge = r'e\s*(\d*)\s*(\d*)\s*(\d*\.\d*)'
+            pattern_end = [r'g\s*\d*\d\s*\d*', r'x\s*\d*']
+            vertices_name = []
+            edge_name = []
+            for line in reader:
+                result_vert = re.search(pattern_vertices, line)
+                result_edge = re.search(pattern_edge, line)
+                result_end = re.search(pattern_end[0], line)
+                if result_vert is not None:
+                    vertices_name.append(result_vert.group(2))
+                if result_edge is not None:
+                    edge_name.append([result_edge.group(1), result_edge.group(2)])
+                if result_end is not None:
+                    if len(vertices_name) > 9:
+                        gx = nx.Graph()
+                        for edge in edge_name:
+                            gx.add_edge(edge[0], edge[1])
+                        list_graphs.append(gx)
+                    vertices_name.clear()
+                    edge_name.clear()
+                if len(list_graphs) == rows * cols:
+                    break
+        ############################################################
         plt.title(name_research, title_size)
         plt.axis('off')
-        for i in range(rows):
-            for j in range(cols):
-                fig.add_subplot(rows, cols, i * rows + j + 1)
-                nx.draw(list_graphs[i][j])
+
+        for i in range(rows * cols):
+                fig.add_subplot(rows, cols, i + 1)
+                nx.draw(list_graphs[i])
         plt.savefig('{}/result/{}.png'.format(data_dir, ind + 1))
