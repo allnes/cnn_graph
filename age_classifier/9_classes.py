dcf3cedaefcd133f1b1a24a1865a80864c8cea54
Nesterov Alexander <nesoldr@mail.ru>
2020-05-25 23:04:15 +0300

    update

--- age_classifier/9_classes.py
+++ age_classifier/9_classes.py
@@ -221,7 +221,7 @@ params['M'] = [2048, C]
 # Optimization.
 params['regularization'] = 5e-4
 params['dropout'] = 0.5
-params['learning_rate'] = 0.001
+params['learning_rate'] = 0.005
 params['decay_rate'] = 0.95
 params['momentum'] = 0.9
 params['decay_steps'] = n_train / params['batch_size']
