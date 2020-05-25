821ca5e97b620e6bcc08f37f781788560bfc83db
Nesterov Alexander <nesoldr@mail.ru>
2020-05-25 23:03:41 +0300

    update

--- age_classifier/9_classes.py
+++ age_classifier/9_classes.py
@@ -187,7 +187,7 @@ plt.spy(A, markersize=2, color='black')
 plt.show()
 
 print('--> Get laplacian matrix')
-graphs, perm = coarsening.coarsen(A, levels=5, self_connections=True)
+graphs, perm = coarsening.coarsen(A, levels=6, self_connections=True)
 X_train = coarsening.perm_data(X_train, perm)
 print(X_train.shape)
 X_val = coarsening.perm_data(X_val, perm)
@@ -214,9 +214,9 @@ assert C == np.unique(y).size
 
 # Architecture.
 params['F'] = [9, 18, 36, 18, 9]
-params['K'] = [18, 12, 6, 12, 18]
-params['p'] = [4, 2, 2, 2, 1]
-params['M'] = [1024, 512, 256, C]
+params['K'] = [18, 18, 18, 18, 18]
+params['p'] = [4, 4, 2, 2, 1]
+params['M'] = [2048, C]
 
 # Optimization.
 params['regularization'] = 5e-4
